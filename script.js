dc4096138f2626dc93a787ca5fbc471a8cb806b3
magic404 <jamesbox3@gmail.com>
2022-12-20 13:43:35 -0800

    substantial coding done

--- script.js
+++ script.js
@@ -1,5 +1,67 @@
 // Assignment code here
 
+// Have some global variables here
+// Following are some booleans
+var includeLowerCase;
+var includeUpperCase;
+var includeNumbers;
+var includeSpecial;
+
+// gloabal variables to hold different valid characters sets 
+var lower = 'abcdefgijklmnopqrstuvwxyz';
+var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbers= '0123456789'
+var special = '!@#$%^&*()_+=-?><,.';
+
+function generatePassword() {
+  console.log("some one clicked generate Password  button");
+
+  var passwordLength = parseInt(prompt("give password length between 8 - 128"));
+  console.log("password Length = ", passwordLength);
+
+  if (passwordLength < 8 || passwordLength >128) {
+    alert("please  choose length between 8 and 128");
+    generatePassword();
+  }
+
+  // Ask for remakining password criteria
+  getPasswordCriteria();
+
+  //Now first build the valid char set from which  to  draw the password from
+  var validChars = '';
+  if (includeLowerCase === true) {
+    validChars += lower;
+    console.log ( 'validChars = ', validChars);
+  }
+  if (includeUpperCase === true) {
+    validChars += upper;
+    console.log ( 'validChars = ', validChars);
+  }
+  if (includeNumbers === true) {
+    validChars += numbers;
+    console.log ( 'validChars = ', validChars);
+  }
+  if (includeSpecial === true) {
+    validChars += special;
+    console.log ( 'validChars = ', validChars);
+  }
+}
+
+function  getPasswordCriteria() {
+  includeLowerCase = confirm('Confirm OK to include lowercase chars in password');
+  includeUpperCase = confirm('Confirm OK to include uppercase chars in password');
+  includeNumbers = confirm('Confirm OK to include numbers in password');
+  includeSpecial = confirm('Confirm OK to include special chars in password')
+
+  if (includeLowerCase === false  &&
+        includeUpperCase === false && 
+        includeNumbers === false &&
+        includeSpecial === false)   {
+          alert(" atleast one type should be selected, please retry");
+          getPasswordCriteria();
+        }
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
